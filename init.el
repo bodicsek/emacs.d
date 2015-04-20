d29bf84d48c272e0f06768212f430677a9d35724
bodicsek <david.nabraczky@gmail.com>
2015-04-20 18:58:56 +0200

    modified - init.el: removed haskell-mode key bindings

--- init.el
+++ init.el
@@ -328,24 +328,6 @@
 (use-package haskell-mode
   :commands haskell-mode
   :init (progn
-          ;; key bingings
-          (eval-after-load 'haskell-mode
-            '(progn
-               (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-               (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-               (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
-               (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
-               (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
-               (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-               (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
-               (define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile)))
-          (eval-after-load 'haskell-cabal
-            '(progn
-               (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-               (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-               (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-               (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
-               (define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile)))
           ;; intendation
           (use-package hi2
             :defer t
