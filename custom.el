010d572300f2589a1fe1204fee71348333aee7ec
bodicsek <bodicsek@nerdmail.co>
2016-12-20 19:43:02 +0100

    Adding a separate custom set file

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elfeed-goodies elfeed iedit company-web-html web-mode windsize which-key w32-browser vlf use-package tide projectile paredit-menu overseer names multiple-cursors magit dired-subtree darkokai-theme company-web cl-lib-highlight bm)))
+ '(safe-local-variable-values (quote ((engine . jsx)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
--- init.el
+++ init.el
@@ -346,18 +346,8 @@
   :commands (restclient-mode)
   :ensure t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (iedit company-web-html web-mode windsize which-key w32-browser vlf use-package tide projectile paredit-menu overseer names multiple-cursors magit dired-subtree darkokai-theme company-web cl-lib-highlight bm)))
- '(safe-local-variable-values (quote ((engine . jsx)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+; ======================== custom set file ===============================
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
