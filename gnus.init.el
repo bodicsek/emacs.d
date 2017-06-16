83b650059d2c9ba47720f3d0c045c89a453246a0
bodicsek <bodicsek@nerdmail.co>
2017-06-16 13:53:05 +0200

    gnus tweaks

--- gnus.init.el
+++ gnus.init.el
@@ -1,8 +1,13 @@
+;;; nnir: search mail with various search engines
+(require 'nnir)
+
 (setq gnus-select-method
       '(nnimap "gmail"
 	       (nnimap-address "imap.gmail.com")
 	       (nnimap-server-port 993)
-	       (nnimap-stream ssl)))
+	       (nnimap-stream ssl)
+               ;; search mail via imap by typing 'G G' in the Group buffer
+               (nnir-search-engine imap)))
 
 (setq starttls-gnutls-program "gnutls-cli")
 
@@ -17,12 +22,29 @@
       smtpmail-smtp-service 587
       smtpmail-debug-info t)
 
+;;; custom article summar with user friendly date
 (setq gnus-summary-line-format "%U%R%z%B%(%[%-23,23f%]%) %-80,80s %&user-date;\n")
-(setq mm-text-html-renderer 'shr)
-(setq gnus-inhibit-images nil)
-(setq gnus-extra-headers '(To Newsgroups X-GM-LABELS))
 
-;; Automate the fetching of mail.
+;;; use eww's shr renderer to display html
+(setq mm-text-html-renderer 'shr)
+(setq shr-color-visible-distance-min 5
+      shr-color-visible-luminance-min 100)
+
+;;; allow inline images
+(setq gnus-inhibit-images nil)
+
+;;; switching off caching
+(setq gnus-agent nil)
+
+;;; labels are handled automatically from 25.1 on
+;;; https://fossies.org/diffs/emacs/24.5_vs_25.1/lisp/gnus/nnimap.el-diff.html
+;; (setq gnus-extra-headers '(To Newsgroups X-GM-LABELS))
+
+;;; search mail via imap using the gmail search terms
+(add-to-list 'nnir-imap-search-arguments '("gmail" . "X-GM-RAW"))
+(setq nnir-imap-default-search-key "gmail")
+
+;; Automate mail fetching.
 ;;(require 'gnus-demon)
 ;; Check for new mail once in every this many minutes.
 ;;(gnus-demon-add-handler 'gnus-demon-scan-news 5 nil)
