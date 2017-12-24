78bc46b47d4ba7ab8db82995ba56eb4677928544
bodicsek <bodicsek@nerdmail.co>
2017-12-24 10:02:01 +0100

    automatic custom.el changes

--- custom.el
+++ custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-async-log-file (f-join (f-canonical (getenv "TEMP")) "dired-async.log"))
  '(package-selected-packages
    (quote
-    (nlinum-hl auto-highlight-symbol ace-window xah-find monokai-theme zenburn-theme powerline moe-theme editorconfig counsel-projectile zoom-window counsel atom-dark-theme idea-darkula-theme dired-narrow eshell-prompt-extras eshell-prompt-extra highlight-indent-guides js2-refactor js2-mode restclient iedit company-web-html windsize w32-browser vlf paredit-menu overseer names dired-subtree darkokai-theme company-web cl-lib-highlight bm)))
+    (lua-mode nlinum-hl auto-highlight-symbol ace-window xah-find monokai-theme zenburn-theme powerline moe-theme editorconfig counsel-projectile zoom-window counsel atom-dark-theme idea-darkula-theme dired-narrow eshell-prompt-extras eshell-prompt-extra highlight-indent-guides js2-refactor js2-mode restclient iedit company-web-html windsize w32-browser vlf paredit-menu overseer names dired-subtree darkokai-theme company-web cl-lib-highlight bm)))
  '(safe-local-variable-values (quote ((engine . jsx)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
